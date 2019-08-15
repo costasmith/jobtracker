59e435f06b49702aaf79ec5e3b0db2454ee1aabd
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-08-15 15:46:13 -0600

    Added name to README and attempted to setup Heroku by editing the server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// Require dotenv becuase it contains my Atlas project link
+// require('dotenv').config();
+
 const mongoose = require('mongoose');
 const db = mongoose.connection;
 
+const PORT = process.env.PORT || 3000;
 
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const PROJECT3_DB = process.env.PROJECT3_DB;
+console.log(process.env);
 
-
+// Fix Depreciation Warnings from Mongoose*
+// May or may not need these depending on your Mongoose version
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 app.get('/', (req,res) => {
   res.send('hiya')
@@ -14,11 +26,20 @@ app.get('/', (req,res) => {
 
 
 //db connection
-mongoose.connect('mongodb://localhost:27017/jobtracker', {useNewUrlParser: true})
+mongoose.connect(PROJECT3_DB, {useNewUrlParser: true})
+
 mongoose.connection.once('open', () => {
   console.log('connected to monkey ...');
 })
+
+// middleware
+//use public folder for static assets
+app.use(express.static('public'));
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+
+
 //listener
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
   console.log( 'Hello Mate! Welcom to port: ')
 });
