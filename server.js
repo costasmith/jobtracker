97a58984d080815b8a20e05fc89ce51ecbc2cbb0
HyoSung Bidol-Lee <hyosung11@me.com>
2019-08-15 17:47:41 -0400

    updates

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
+// dependencies
+const express = require('express')
 const app = express();
-const port = 3000;
-const mongoose = require('mongoose');
-const db = mongoose.connection;
+const port = 3000
+const mongoose = require('mongoose')
+const db = mongoose.connection
 
 
-
+// database connection
+const PROJECT3_DB = process.env.PROJECT3_DB
 
 
 app.get('/', (req,res) => {
@@ -13,12 +15,13 @@ app.get('/', (req,res) => {
 })
 
 
-//db connection
-mongoose.connect('mongodb://localhost:27017/jobtracker', {useNewUrlParser: true})
+// connect to Mongo 
+mongoose.connect(PROJECT3_DB, {useNewUrlParser: true})
 mongoose.connection.once('open', () => {
-  console.log('connected to monkey ...');
+  console.log('connected to monkey ...')
 })
+
 //listener
 app.listen(3000, () =>{
   console.log( 'Hello Mate! Welcom to port: ')
-});
+})
