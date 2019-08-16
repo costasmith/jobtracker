a800424557e26d36268531cd8955f61cac090eee
HyoSung Bidol-Lee <hyosung11@me.com>
2019-08-16 13:34:04 -0400

    working on edit route

--- public/js/app.js
+++ public/js/app.js
@@ -23,12 +23,49 @@ app.controller('JobController', ['$http', function ($http) {
       }
     }).then(
       function (response) {
-        console.log(response)
-        // controller.getJobs()
+        console.log(response.data)
+        controller.getJobs()
       }, function (error) {
         console.log(error);
       }
     )
   }
-  // this.getJobs()
+
+  // get/read route
+  this.getJobs = function () {
+    $http({
+      method: 'GET',
+      url: '/jobs'
+    }).then(function (response) {
+      controller.jobs = response.data
+      console.log(controller.jobs);
+    }, function (error) {
+        console.log(error);
+    })
+  }
+
+  this.editJob = function (job) {
+    $http({
+      method: 'PUT',
+      url: '/jobs' + job._id,
+      data: {
+        position: this.position,
+        company: this.company,
+        applied: this.applied,
+        resume: this.resume,
+        letter: this.letter,
+        followedUp: this.followedUp,
+        salary: this.salary,
+        url: this.url,
+        stage: this.stage,
+        status: this.status
+      }
+    }).then(function () {
+      controller.getJobs()
+      controller.indexOfEditFormToShow = null
+    })
+  }
+
+
+  this.getJobs()
 }])
