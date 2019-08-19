8c68b4109e7e8d3fcd0d83227a3197b7da57c8eb
DaCosta Smith <dacostasmith@macs-MacBook-Air.local>
2019-08-19 12:24:26 -0400

    way better css

--- public/js/app.js
+++ public/js/app.js
@@ -108,6 +108,8 @@ app.controller('JobController', ['$http', function ($http) {
         function (response) {
             console.log('updated job received from job controller: ', response);
         controller.replaceJob(response)  // delete the old version, add the new
+        controller.indexOfEditFormToShow = null;
+
 
     }, function(error) {
         console.log(error);
